c1d1e65f51b8cf63c96cd325bc48cf75f1c43f46
KaWaIINa <81364308+millove123@users.noreply.github.com>
2022-05-19 10:50:11 +0700

    save

--- lab1/app.js
+++ lab1/app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 
 
 
@@ -11,9 +11,21 @@ app.get("/",async (req,res)=>{
     await res.status(200).send("Good")
     
 })
-app.use('*', (req, res)=>{
-    res.send('what???', 404);
-  });
+// app.use('*', (req, res)=>{
+//     res.status(404).send('what???');
+//   });
+
+app.post("/user",(req,res)=>{
+    const id  = req.body.name
+    console.log(req.body)
+    if( id == "Mew"){
+        res.status(200).send("Good")
+    }
+    else{
+        res.status(400).send("Not")
+    }
+   
+})
 
 app.listen(3000,()=>{
     console.log("Sever Start At -----> 3000");
